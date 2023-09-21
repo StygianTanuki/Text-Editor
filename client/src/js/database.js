522bca569a3945c1ea3f97c8a51731ae3e7a5f28
Felix Rivera <darkflameice64@gmail.com>
2023-09-21 18:36:19 -0400

    Finished the TODOs in the JS

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const req =  store.add({id: "1", value: content});
+  const result = await req;
+  console.log('Content added to the database:', result.value);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const textDb = await openDB("jate", 1);
+  const tx = textDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,25 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+    installPrompt = event;
+    butInstall.style.visibility = "visible";
+  });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Implemented a click event handler on the `butInstall` element
+butInstall.addEventListener("click", async (event) => {
+    if (!installPrompt) {
+      return;
+    }
+  
+    const result = await installPrompt.prompt();
+    installPrompt = null;
+    butInstall.setAttribute("disabled", true);
+    butInstall.textContent = "Installed!";
+  });
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Added a handler for the `appinstalled` event
+window.addEventListener("appinstalled", (event) => {
+    console.log("appinstalled", event);
+  });
